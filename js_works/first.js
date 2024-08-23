c0b572c2bcb0e7a755a5cf2fbcbb310407162e0b
hithesh143life@gmail.com <1bytecode.git@gmail.com>
2024-08-23 19:52:33 +0530

    works on js-promts and strings

---
+++ js_works/first.js
@@ -0,0 +1,27 @@
+//-------------------------------------------------------------------------
+// let marks = alert("good morning please ensure to subscribe ");
+// let student = {
+//     name: "chandu ",
+//     age : 22,
+//     role : "ceo",
+//     company_name : "1week.org",
+// };
+// console.log(student.company_name);
+// for ( let key in  student ){
+//     console.log("key atribute will b-",key);
+//     console.log("key atribute values -",student[key]);
+// }-
+//--guess the number game ---------------------------------------------------------------------
+// let num =28;
+// let valu = prompt("guess the number ");
+// while(valu != num){
+
+//     valu = prompt("no, please enter the correct number");
+// }
+// console.log(" congragulation , your guess is correct ");
+let comp1="@";
+let comp2 = prompt("Enter your full name");
+// console.log(comp2.length);
+let comp3= comp2.length;
+let username = comp1+comp2+comp3;
+console.log(" Your user name will be ---",username);
