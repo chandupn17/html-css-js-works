42ac01b9b3fae9d1f82e5d8cadd7c8d3ab2c4c77
hithesh143life@gmail.com <1bytecode.git@gmail.com>
2024-12-07 22:31:10 +0530

    working on api

--- weather/weather.js
+++ weather/weather.js
@@ -1,30 +1,52 @@
 const apiKey = "f49a63ca184298cf290066f7e836eb38";
-const Base_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore";
-const input = document.querySelector("#inputText");
+const Base_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+// const inputData = document.querySelector("inputText");
+const search = document.querySelector("search-icon");
+const btn = document.querySelector("#btn");
 
-const getdata = async ()=>{
+
+
+
+// const input = document.querySelector("inputText");
+
+const getdata = async (cityname)=>{
     try{
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=f49a63ca184298cf290066f7e836eb38");
+       // const cityname = "bengaluru";
+    const response = await fetch(Base_url+cityname+`&appid=${apiKey}`);
     if(!response){
     console("error in fetching the data ");
     }
     const data = await response.json();
-    console.log(data);
-    for(let dataItm in data.wind){
-        if(data.wind === data.wind.deg){
-            console.log("hello gdsc");
-            console.log(dataItm);
-        }
-    }
-    // console.log(data.wind);
- 
+   console.log(data.main.humidity);
+   
+    console.log("the total api details willl bve " + data);
+    console.log(data.main);
 }
     catch(error){
         console.log(error);
     }
-}
- 
-getdata();
+} 
+
+btn.addEventListener("click", (btn)=>{
+    btn.preventDefault();
+    console.log("data is fetching . . . . . . ");
+    //The error suggests that the JavaScript is trying to access
+    // an element (cityInput) that doesn’t exist in the DOM at 
+    //the time the script is executed. This often happens if the 
+    //script is loaded before the HTML is fully rendered.
+    const inputData = document.querySelector("#inputText");
+   // console.log( document.querySelector("inputText"));
+     const city = inputData.value.trim();
+     if(!city){
+        console.log("input city is not fetched ");
+     }else{
+        console.log("The inputed data will be "+ city);
+
+     }
+     
+    getdata(city);
+    
+});
 
 
 
