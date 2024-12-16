ab39d0259cd386bd154fa0a6839eb1cf2ae12fe6
hithesh143life@gmail.com <1bytecode.git@gmail.com>
2024-12-16 20:59:43 +0530

    final weather app

--- weather/weather.js
+++ weather/weather.js
@@ -3,9 +3,12 @@ const Base_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q
 // const inputData = document.querySelector("inputText");
 const search = document.querySelector("search-icon");
 const btn = document.querySelector("#btn");
-
-
-
+const img = document.querySelector("#img");
+const city= document.querySelector("#city");
+const tem = document.querySelector("#degree");
+const weatherimg = document.querySelector("#img");
+const humid = document.querySelector("#humid");
+const wind = document.querySelector("#wind");
 
 // const input = document.querySelector("inputText");
 
@@ -17,19 +20,41 @@ const getdata = async (cityname)=>{
     console("error in fetching the data ");
     }
     const data = await response.json();
-   console.log(data.main.humidity);
+
+let temp = Math.round(data.main.temp);
+
+if (data.weather[0].main == "Clouds") {
+    weatherimg.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherimg.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherimg.src = "images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherimg.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherimg.src = "images/mist.png";
+  }else if (data.weather[0].main == "Haze") {
+    weatherimg.src = "images/mist.png";
+  }
+     
+    city.innerHTML = cityname;
+    tem.innerHTML = temp+ "°C";
+    wind.innerHTML = data.wind.speed + "km/h";
+    humid.innerHTML = data.main.humidity + "%";
+
+
    
-    console.log("the total api details willl bve " + data);
-    console.log(data.main);
+    // console.log(data);
 }
     catch(error){
+        alert("enter the city name correctly");
         console.log(error);
     }
 } 
 
 btn.addEventListener("click", (btn)=>{
     btn.preventDefault();
-    console.log("data is fetching . . . . . . ");
+    // console.log("data is fetching . . . . . . ");
     //The error suggests that the JavaScript is trying to access
     // an element (cityInput) that doesn’t exist in the DOM at 
     //the time the script is executed. This often happens if the 
@@ -41,7 +66,6 @@ btn.addEventListener("click", (btn)=>{
         console.log("input city is not fetched ");
      }else{
         console.log("The inputed data will be "+ city);
-
      }
      
     getdata(city);
